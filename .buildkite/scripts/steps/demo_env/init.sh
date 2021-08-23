e71c5af42de3f3132dc7444cddb8adb703f77cf5
Brian Seeders <brian.seeders@elastic.co>
2021-08-23 13:53:43 -0400

    Use docker load instead of import

--- .buildkite/scripts/steps/demo_env/init.sh
+++ .buildkite/scripts/steps/demo_env/init.sh
@@ -8,10 +8,14 @@ source "$(dirname "${0}")/config.sh"
 
 echo '--- Import and publish Elasticsearch image'
 
+mkdir -p target
+
 export ES_IMAGE="gcr.io/elastic-kibana-184716/demo/elasticsearch:$DEPLOYMENT_NAME-$(git rev-parse HEAD)"
 
 DOCKER_EXPORT_URL=$(curl https://storage.googleapis.com/kibana-ci-es-snapshots-daily/$DEPLOYMENT_VERSION/manifest-latest-verified.json | jq -r '.archives | .[] | select(.platform=="docker") | .url')
-docker import "$DOCKER_EXPORT_URL" "$ES_IMAGE"
+curl "$DOCKER_EXPORT_URL" > target/elasticsearch-docker.tar.gz
+docker load < target/elasticsearch-docker.tar.gz
+docker tag "docker.elastic.co/elasticsearch/elasticsearch:$DEPLOYMENT_VERSION-SNAPSHOT" "$ES_IMAGE"
 docker push "$ES_IMAGE"
 
 echo '--- Prepare yaml'
